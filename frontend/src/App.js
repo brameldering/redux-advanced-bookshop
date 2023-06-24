4ccdc6eb7d580a2f59e8c7c6b73383fa995d8cbb
brameldering <bram.eldering@gmail.com>
2023-06-24 16:25:25 +0200

    Saving cart with status and error handling using UI slice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,40 @@
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useEffect, Fragment } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
+import Notification from "./components/UI/Notification";
 import Products from "./components/Shop/Products";
+import { uiActions } from "./store/ui-slice";
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const notification = useSelector((state) => state.ui.notification);
   const cartUser = useSelector((state) => state.cart.user);
   const cartItems = useSelector((state) => state.cart.items);
 
-  const [isUpdating, setIsUpdating] = useState(false);
+  let isUpdating = notification && notification.status === "pending";
 
   console.count("App-start");
 
   useEffect(() => {
-    console.warn("--> App - useEffect called");
-    console.count("useEffect");
+    console.count("--> App useEffect");
     const writeCart = async () => {
-      setIsUpdating(true);
+      console.log("writeCart");
+      // setIsUpdating(true);
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Saving...",
+          message: "Saving cart data",
+        })
+      );
+
+      await new Promise((r) => setTimeout(r, 1000));
+
       const response = await fetch("http://localhost:8000/api/carts", {
         method: "PUT",
         headers: {
@@ -30,30 +46,59 @@ function App() {
         }),
       });
       if (!response.ok) {
-        console.log(response);
-        throw new Error("Request failed!");
+        console.warn(response);
+        throw new Error(`Sending cart data failed! (${response.status} - ${response.statusText})`);
       }
-      const updateResponse = await response.json();
-      console.log("-> App - updateResponse");
-      console.log(updateResponse);
-      // await new Promise((r) => setTimeout(r, 500));
-      setIsUpdating(false);
+      const responseData = await response.json();
+      console.log("-> App - responseData");
+      console.log(responseData);
+      // setIsUpdating(false);
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success",
+          message: "Saved cart data succesfully",
+        })
+      );
     };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
 
     if (cartUser && cartItems.length > 0) {
       writeCart().catch((error) => {
         console.error("Error in writeCart ");
-        console.log(error);
+        console.log(error.message);
+        dispatch(
+          uiActions.showNotification({
+            status: "error",
+            title: "Error!",
+            message: error.message,
+          })
+        );
       });
       console.log("--> App - after writeCart");
     }
-  }, [cartItems, cartUser]);
+  }, [cartItems, cartUser, dispatch]);
+
+  console.info("--> App isUpdating: " + isUpdating);
 
   return (
-    <Layout isUpdating={isUpdating}>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout isUpdating={isUpdating}>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
